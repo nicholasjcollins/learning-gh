0ce83631219251dc8d3a902ebc7e856d5bd335ca
Nicholas Collins <nicholasjcollins@me.com>
2021-03-16 12:05:36 -0400

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Nicholas Collins on 3/16/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
